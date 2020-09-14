a2ce2f9a3853a6f4fa870b4422b57f80d0462c93
Daniel <mabadejedaniel1@gmail.com>
2020-09-13 21:45:23 -0700

    added take command

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,6 @@
-import pyttsx3
-
+import pyttsx3 
+import datetime
+import speech_recognition as sr
 engine = pyttsx3.init()
 
 
@@ -23,4 +24,29 @@ def date():
     speak(month)
     speak(year)
 
-time()
+
+def wishme():
+    speak("Welcome back sir!")
+    
+    speak("Jarvis at your service, How may i HELP YOU")
+    
+    
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        
+    try:
+        print("Recognizing")
+        query  = r.recognize_google(audio language='en-in')
+        print(query)
+        
+        
+    except exception as e:
+        print(e)
+        
+    return query
+
+take()
