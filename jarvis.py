01e9fb8f3670bc5ea78365ee433b933d2d218df5
Daniel <mabadejedaniel1@gmail.com>
2020-09-13 21:06:19 -0700

    created speak function

--- jarvis.py
+++ jarvis.py
@@ -2,6 +2,11 @@ import pyttsx3
 
 engine = pyttsx3.init()
 
-engine.say('Hello This is JARVIS')
 
-engine.runAndWait()
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    
+    
+speak("this is jarvis AI assistant")
